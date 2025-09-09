062d1948dac5e67b218bf52fa1fb633c9805fb03
ypavanr <ypavanreddy190@gmail.com>
2025-09-10 02:55:57 +0530

    initial commit

---
+++ frontend/src/App.svelte
@@ -0,0 +1,32 @@
+<main>
+	<h1><span>STACK</span> <span class="black">BLOCK</span></h1>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	h1 span:first-child {
+		color: #ff3e00; 
+	}
+
+	h1 .black {
+		color: black; 
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
